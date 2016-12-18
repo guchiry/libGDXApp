7ca5027378ab378d6baf2c19657999fac6e59fd0
guchiry <wisteria_la_vida@yahoo.co.jp>
2016-12-18 22:48:55 +0900

    ゲーム難易度の調整

--- src/com/mygdx/game/Setting.java
+++ src/com/mygdx/game/Setting.java
@@ -9,10 +9,10 @@ public class Setting {
 	public static final float DICISION_FADE_TIME = 0.5f;
 
 	public static final float MAX_DISTANCE = 1000f;
-	public static final float SUCCESS_DISTANCE = 25f;
-	public static final float MISS_DISTANCE = 45f;
+	public static final float SUCCESS_DISTANCE = 20f;
+	public static final float MISS_DISTANCE = 35f;
 	public static final float GOOD_DISTANCE = SUCCESS_DISTANCE;
-	public static final float GREAT_DISTANCE = 15f;
+	public static final float GREAT_DISTANCE = 10f;
 	public static final float PERFECT_DISTANCE = 5f;
 
 	public static final int ALL_NUMBER = 1;
