1b09ba0069135deadf3352e816f51acbc10f9f1e
guchiry <wisteria_la_vida@yahoo.co.jp>
2016-12-21 21:36:18 +0900

    0が表示できないバグを修正

--- src/com/mygdx/game/NumberList.java
+++ src/com/mygdx/game/NumberList.java
@@ -6,11 +6,11 @@ public class NumberList {
 	private Array<Integer> list = new Array<Integer>();
 
 	public NumberList(int num){
-		while(num != 0){
+		do{
 			int i = num%10;
 			list.add(i);
 			num = num/10;
-		}
+		}while(num != 0);
 		list.reverse();
 	}
 
