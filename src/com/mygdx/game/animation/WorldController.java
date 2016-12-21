e0dcc262a16bfded44e55500d352b62ef431b00a
Peace <pedid.y-se_os_dara@outlook.jp>
2016-12-21 15:50:32 +0900

    アニメ追加終了。しかし動かせないのでとりあえずコメントアウト

--- src/com/mygdx/game/animation/WorldController.java
+++ src/com/mygdx/game/animation/WorldController.java
@@ -29,6 +29,7 @@ public class WorldController {
 	//1:shinonreal
 	public void update(float delta) {
 		time += delta;
+		//カエデ、シノン、リアル
 		fadeOutControll(0,2,delta,0);
 		rotateControll(3,4,Direction.RIGHT,0);
 		rotateControll(3,4,Direction.LEFT,1);
@@ -47,16 +48,16 @@ public class WorldController {
 			world.getDispHeroList().add(new KaedeGame(new Vector2(310, 150)));
 			world.getDispHeroList().add(new ShinonGame(new Vector2(400, 150)));
 		}
+		//カエデ、シノン、ゲーム内
 		fadeOutControll(13,15,delta,2);
 		rotateControll(15,16,Direction.RIGHT,0);
 		rotateControll(15,16,Direction.LEFT,1);
 		feelingControll(16,19,FeelType.B,1);//敵
 		feelingControll(17,19,FeelType.B,0);//〇
 		jumpControll(17,0);
-		moveControll(20,24,Direction.RIGHT,0);
-		moveControll(20,24,Direction.RIGHT,1);
+		moveControll(20,25,Direction.RIGHT,0);
+		moveControll(20,25,Direction.RIGHT,1);
 		fadeInControll(23,25,delta,3);
-
 		if(time >= 25 && 27 > time){
 			world.getDispHeroList().removeRange(0, 1);
 			world.getDispHeroList().add(new KaedeGame(new Vector2(360, 70)));
@@ -64,13 +65,50 @@ public class WorldController {
 //			world.getDispHeroList().add(new Goblin(new Vector2(360, 150)));
 		}
 		rotateControll(25,27,Direction.BACK,0);
+		//カエデ、シノン、ゴブリン、ゲーム内戦闘
 		fadeOutControll(25,27,delta,4);
 		tackleControll(27,Direction.BACK,0);
 		tackleControll(28,Direction.FRONT,1);
-
-//		tackleControll(13,Direction.RIGHT,1);
-//		System.out.println(time);
-
+		fadeInControll(28,30,delta,5);
+		if(time >= 30 && 32 > time){
+			world.getDispHeroList().removeRange(0, 1);
+			world.getDispHeroList().add(new KaedeGame(new Vector2(310, 150)));
+			world.getDispHeroList().add(new VisnuGame(new Vector2(440, 150)));
+		}
+		rotateControll(30,32,Direction.RIGHT,0);
+		rotateControll(30,32,Direction.LEFT,1);
+		//カエデ、ヴィシュヌ、ゲーム内
+		fadeOutControll(30,32,delta,6);
+		moveControll(32,33,Direction.LEFT,1);
+		jumpControll(33,1);
+		feelingControll(34,35,FeelType.B,0);//〇
+		moveControll(35,40,Direction.RIGHT,0);
+		moveControll(35,40,Direction.RIGHT,1);
+		fadeInControll(39,41,delta,7);
+		if(time >= 41 && 43 > time){
+			world.getDispHeroList().removeRange(0, 1);
+			world.getDispHeroList().add(new KaedeGame(new Vector2(360, 70)));
+			world.getDispHeroList().add(new ShinonGame(new Vector2(360, 150)));
+			world.getDispHeroList().add(new VisnuGame(new Vector2(360, 230)));
+		}
+/*
+		rotateControll(41,43,Direction.BACK,2);
+		//カエデ、ヴィシュヌ、シノン、ゲーム内戦闘
+		fadeOutControll(41,43,delta,8);
+		feelingControll(44,46,FeelType.B,0);//驚き
+		feelingControll(44,46,FeelType.B,2);//驚き
+		feelingControll(47,48,FeelType.B,1);//無言
+		tackleControll(49,Direction.FRONT,1);
+		feelingControll(50,51,FeelType.B,2);//-10p
+		feelingControll(51,52,FeelType.C,0);//驚き
+		feelingControll(51,52,FeelType.C,2);//驚き
+		feelingControll(52,53,FeelType.A,2);//泣き
+		jumpControll(52,2);
+		feelingControll(53,54,FeelType.B,1);//無言
+		if(time >= 52){
+			world.getDispHeroList().removeRange(1, 1);
+		}
+*/
 	}
 
 	public void fadeInControll(float startTime, float endTime, float delta, int count){
@@ -120,7 +158,7 @@ public class WorldController {
 
 	public void jumpControll(float startTime,int listNumber){
 		Hero hero = world.getDispHeroList().get(listNumber);
-		if(time >= startTime && startTime+0.643 > time){
+		if(time >= startTime && startTime+0.642 > time){
 			hero.setState(hero.state.JUMP);
 			world.jump(hero);
 		}
